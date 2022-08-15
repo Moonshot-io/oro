c4c15c65b0241851272ca13fbcb0e4f39481adbc
Bethany Jones <betpetjones@gmail.com>
2022-08-14 21:35:17 -0500

    fixing event cards due to old code

--- client/components/EventCards.tsx
+++ client/components/EventCards.tsx
@@ -36,11 +36,6 @@ const EventCards = ({ events }) => {
   // }, []);
 
   const getDetails = (id) => {
-<<<<<<< HEAD
-    console.log('navigate', id);
-=======
-    // console.log('navigate', id);
->>>>>>> aeecc04 (commented out console logs, updated routing to details)
     navigate(`/details/?id=${id}`);
   };
 
--- client/pages/EventListings.tsx
+++ client/pages/EventListings.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import EventCardDetails from '../components/EventCardDetails';
+import EventCardDetailDetails from '../components/EventCardDetailDetails';
+
+import eventDummy from '../../server/database/data/eventDummy';
 
 import eventDummy from '../../server/database/data/eventDummy';
 const EventListings: React.FC = () => {
@@ -28,7 +30,7 @@ const EventListings: React.FC = () => {
     axios
       .get('/events/list', { params: { keyword: keyword } })
       .then((responseObj) => {
-        // console.log('GETEVENTS RESPONSEOBJ', responseObj);
+        console.log('GETEVENTS RESPONSEOBJ', responseObj);
         setEvents(responseObj.data.events);
       })
       .catch((err) => console.error(err));
@@ -36,7 +38,7 @@ const EventListings: React.FC = () => {
 
   useEffect(() => {
     getEvents();
-    // console.log(keyword);
+    console.log(keyword);
     console.log('EVENTS', events);
   }, []);
 
@@ -63,7 +65,14 @@ const EventListings: React.FC = () => {
   return (
     <div>
       <div>Hello EventListings</div>
-      <input placeholder='enter keywords here (e.g. artist, event, venue, city, state, date...'></input>
+      <input
+        placeholder='enter keywords here (e.g. artist, event, venue, city, state, date...'
+        type='text'
+        id=''
+        onChange={handleChange}
+        value={keyword}
+        onKeyDown={enterClick}
+      ></input>
       <div>
         {events.map((event) => (
           <EventCardDetails events={events} event={event} key={event.eventId} />
