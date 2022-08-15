0fe6ea49a826d0d80952dc01933c49bdef3309c0
Bethany Jones <betpetjones@gmail.com>
2022-08-14 21:34:48 -0500

    Update EventListings.tsx

--- client/pages/EventListings.tsx
+++ client/pages/EventListings.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+<<<<<<< HEAD
 import EventCardDetailDetails from '../components/EventCardDetailDetails';
 
 import eventDummy from '../../server/database/data/eventDummy';
+=======
+import EventCardDetails from '../components/EventCardDetails';
+>>>>>>> c56d926 (Update EventListings.tsx)
 
 import eventDummy from '../../server/database/data/eventDummy';
 const EventListings: React.FC = () => {
+
   // const [events, setEvents] = useState([]);
 
   // BELOW FUNCTION TO BE USED TO REMOVE PUNCTUATION FROM SEARCH QUERY
@@ -18,7 +23,11 @@ const EventListings: React.FC = () => {
   // EVENT LISTING URL
   // https://www.ticketmaster.com/event/${eventIdHere}
 
+<<<<<<< HEAD
   const [keyword, setKeyword] = useState("jane's addiction");
+=======
+  const [ keyword, setKeyword ] = useState('jane\'s addiction');
+>>>>>>> c56d926 (Update EventListings.tsx)
   const [events, setEvents] = useState(eventDummy);
 
   const getEvents = () => {
@@ -27,13 +36,17 @@ const EventListings: React.FC = () => {
     // .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, " ")
     // .replace(/\s{1,}/g, "+")
     // .toLowerCase();
+<<<<<<< HEAD
     axios
       .get('/events/list', { params: { keyword: keyword } })
+=======
+    axios.get('/events/list', { params: { keyword: keyword } })
+>>>>>>> c56d926 (Update EventListings.tsx)
       .then((responseObj) => {
         console.log('GETEVENTS RESPONSEOBJ', responseObj);
         setEvents(responseObj.data.events);
       })
-      .catch((err) => console.error(err));
+      .catch(err => console.error(err));
   };
 
   useEffect(() => {
@@ -52,6 +65,7 @@ const EventListings: React.FC = () => {
     setKeyword(e.target.value);
   };
 
+<<<<<<< HEAD
   const enterClick = (e) => {
     if (e.keyCode === 13) {
       getEvents();
@@ -77,6 +91,22 @@ const EventListings: React.FC = () => {
         {events.map((event) => (
           <EventCardDetails events={events} event={event} key={event.eventId} />
         ))}
+=======
+  return (
+    <div>
+      <div>Hello EventListings</div>
+      <input placeholder='enter keywords here (e.g. artist, event, venue, city, state, date...' type='text' id='' onChange={ handleChange } value={keyword} onKeyDown={enterClick}></input>
+      <div>
+        {
+          events.map(event => (
+            <EventCardDetails
+              events={ events }
+              event={event}
+              key={event.eventId}
+            />
+          ))
+        }
+>>>>>>> c56d926 (Update EventListings.tsx)
       </div>
     </div>
   );
