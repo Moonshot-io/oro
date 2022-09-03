a6759ddf246ad39014c95d6eb32b498016e1a728
Bethany Jones <betpetjones@gmail.com>
2022-09-02 20:03:23 -0500

    reduce event card size, update more info save buttons

--- client/pages/EventListings.tsx
+++ client/pages/EventListings.tsx
@@ -2,19 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import EventCardDetails from '../components/EventCardDetails';
 import eventDummy from '../../server/database/data/eventDummy';
-import { CssTextField } from '../styles/material';
-import SearchIcon from '@mui/icons-material/Search';
-import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import { purple } from '@mui/material/colors';
+import { CssTextField, Grid, SearchIcon, Button, Styled, purple, Grid } from '../styles/material';
 
 const fontColor = {
   style: { color: '#9B27B0' }
 };
 
-const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+const ColorButton = Styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
   backgroundColor: purple[500],
   '&:hover': {
@@ -52,24 +46,30 @@ const EventListings: React.FC = () => {
 
   return (
     <div>
+      <h1>Search for Events</h1>
       <br/>
-      <div>
-        <CssTextField InputLabelProps={fontColor} inputProps={fontColor}
-          sx={{ mb: '15px'}} id="keywordSearch" color="secondary" label="search events" type='text' onChange={ handleChange } value={keyword} onKeyDown={enterClick} />
-        <div>
-          <ColorButton onClick={getEvents} size='large' variant="contained"><SearchIcon /></ColorButton>
-        </div>
-      </div><br/>
-      <div>
+      <Grid container spacing={0}
+  direction="column"
+  alignItems="center"
+  justifyContent="center"
+  style={{ maxHeight: '50vh' }}>
+      <Grid xs={6}><CssTextField InputLabelProps={fontColor} inputProps={fontColor}
+          sx={{ mb: '15px'}} id="keywordSearch" color="secondary" label="search events" type='text' onChange={ handleChange } value={keyword} onKeyDown={enterClick}/>
+          <ColorButton mt="50%"onClick={getEvents} size='large' variant="contained"><SearchIcon /></ColorButton>
+        </Grid>
+      </Grid><br/>
+      <Grid container spacing={2}>
         {
           events.map((event, index) => (
+            <Grid item xs={12} md={6} lg={4}>
             <EventCardDetails
               event={event}
               key={`event${index}`}
             />
+            </Grid>
           ))
         }
-      </div>
+      </Grid>
     </div>
   );
 };
