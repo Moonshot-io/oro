a42375a9ab32a20a1ca81376b5c0a505b639f761
Vincent Tong <vincent.khang.tong@gmail.com>
2022-09-05 11:59:27 -0500

    comment label added. padding adjusted

--- client/components/Comments.tsx
+++ client/components/Comments.tsx
@@ -89,7 +89,7 @@ const Comments: React.FC<UserPictureProps> = ({photo}) => {
         );
       })}
 
-      <CssTextField onKeyPress={(e) => e.key === 'Enter' && handleSend()} InputLabelProps={fontColor} multiline={true} inputProps={{fontColor, maxLength: 150}} sx={{ ml: '15px', mb: '40px', mt: '20px'}} color="secondary" size='small' onChange={(e) => handleComment(e)} value={message}/>
+      <CssTextField placeholder='add comment' onKeyPress={(e) => e.key === 'Enter' && handleSend()} InputLabelProps={fontColor} multiline={true} inputProps={{fontColor, maxLength: 150}} sx={{ ml: '15px', mb: '40px', mt: '20px'}} color="secondary" size='small' onChange={(e) => handleComment(e)} value={message}/>
       <Fab variant='extended' type='submit' onClick={handleSend}
         sx={{bgcolor: iconColors, ml: '20px', mt: '15px'}}><SendIcon sx={{ color: inverseMode }}/></Fab>
     </div>
