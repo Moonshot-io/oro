29857824a283d0771044ba2df368300d216d296a
Bethany Jones <betpetjones@gmail.com>
2022-09-06 12:01:50 -0500

    Event listings: Dropdown working, back button on artist (#169)
    * reduce event card size, update more info save buttons
    
    * event details title changes
    
    * updated event listing filter, added diff color for comments
    
    * dropdown rendering properly
    
    * added no event found
    
    * back button overlay in artist banner

--- client/components/Dropdown.tsx
+++ client/components/Dropdown.tsx
@@ -5,15 +5,18 @@ import {Box, MenuItem, InputLabel, FormControl, Select} from '../styles/material
 // filter by city
 // filter by date
 export default function Dropdown({eventList, updateEvents}) {
-  const [events, setEvents] = React.useState(eventList);
+  const [events, setEvents] = React.useState([...eventList]);
   const [label, setLabel] = React.useState('city');
-  const [age, setAge] = React.useState('');
   const [city, setCity] = React.useState('');
 
   const handleChange = (event: SelectChangeEvent) => {
     setCity(event.target.value as string);
-    updateEvents(city);
+    const newCity = event.target.value;
+    updateEvents(newCity);
   };
+
+  React.useEffect(()=>{
+  }, [events]);
 
   return (
     <Box sx={{ minWidth: 120 }}>
@@ -22,14 +25,19 @@ export default function Dropdown({eventList, updateEvents}) {
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={age}
+        //   value={age}
           onChange={handleChange}
         >
-          {events.map((event)=>{
-            const city = event.venueInfo[0].city;
-            console.log(city);
-            return <MenuItem value={city}>{city}</MenuItem>
-          })}
+         <MenuItem value="all">All</MenuItem>
+          {(()=>{
+            const cities = {};
+            return eventList.map((event)=>{
+            const cityName = event.venueInfo[0].city;
+            if(!cities[cityName]){
+                cities[cityName] = cityName;
+                return <MenuItem value={cityName}>{cityName}</MenuItem>
+            }
+          })})()}
         </Select>
       </FormControl>
     </Box>
