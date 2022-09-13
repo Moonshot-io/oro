4ce7e3c0ae1f404dced16122e7ba5c630ee18049
b.chau <brianchau7979@gmail.com>
2022-09-13 16:11:58 -0500

    Changed popper to popover & made userEvent props optional (#186)
    * updating UserEvents schema to save event info
    
    * refactored profile events to save and retrieve from DB & fixed some console errors
    
    * putting delete and edit button in moreVert button
    
    * changed popper to popover & made userEvents props optional
    
    * correcting merge conflicts

--- client/components/Theme.tsx
+++ client/components/Theme.tsx
@@ -359,9 +359,9 @@ color: ${(props) => props.theme.palette.primary.contrastText};
   padding-bottom: 2px;
 }
 
-
 .css-11lq3yg-MuiGrid-root {
   padding: 10px 12.5px 0px 0px;
+}
 
 .notificationIMG {
   max-width: 100%;
@@ -371,6 +371,14 @@ color: ${(props) => props.theme.palette.primary.contrastText};
 .notificationBody {
   margin: auto;
 
+}
+
+.css-1a4m082-MuiCardContent-root:last-child {
+  padding-bottom: 0px;
+}
+
+.css-1drgtl0-MuiButtonBase-root-MuiIconButton-root {
+  font-size: 1rem;
 }
 
 `;
