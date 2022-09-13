baeea58c3d6ccba6d4fa7b8baa76cfaa05551f3c
b.chau <brianchau7979@gmail.com>
2022-09-13 11:45:57 -0500

    putting delete and edit button in moreVert button

--- client/components/Theme.tsx
+++ client/components/Theme.tsx
@@ -359,6 +359,10 @@ color: ${(props) => props.theme.palette.primary.contrastText};
   padding-bottom: 2px;
 }
 
+.css-11lq3yg-MuiGrid-root {
+  padding: 10px 12.5px 0px 0px;
+}
+
 `;
 interface Props {
   children?: ReactNode
